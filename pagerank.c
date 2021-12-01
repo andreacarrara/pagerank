ad09b682c98eab4667e096b0e8e6e1132adce2f7
Andrea Carrara <andreacarrar@icloud.com>
2021-12-01 08:48:16 +0100

    Fix input

--- pagerank.c
+++ pagerank.c
@@ -17,9 +17,9 @@ int main() {
 	int num_pages = 2;
 	do {
 		if (num_pages < 2)
-			printf("Your web have too few pages, try 2 or more.\n");
+			printf("Your web has too few pages, try 2 or more.\n");
 		else if (num_pages > MAX_PAGES)
-			printf("Your web have too many pages, try %d or less.\n", MAX_PAGES);
+			printf("Your web has too many pages, try %d or less.\n", MAX_PAGES);
 		printf("How many pages does your web have? ");
 		scanf("%d", &num_pages);
 	} while (num_pages < 2 || num_pages > MAX_PAGES);
@@ -35,9 +35,9 @@ int main() {
 		int num_links = 1;
 		do {
 			if (num_links < 1)
-				printf("Page %d have too few links, try 1 or more.\n", i + 1);
+				printf("Page %d has too few links, try 1 or more.\n", i + 1);
 			else if (num_links > num_pages - 1)
-				printf("Page %d have too many links, try %d or less.\n", i + 1, num_pages - 1);
+				printf("Page %d has too many links, try %d or less.\n", i + 1, num_pages - 1);
 			printf("How many links does page %d have? ", i + 1);
 			scanf("%d", &num_links);
 		} while (num_links < 1 || num_links > num_pages - 1);
@@ -50,10 +50,12 @@ int main() {
 					printf("Page %d does not exist, try again.\n", page_num);
 				else if (page_num == i + 1 && has_repeated)
 					printf("A page can't link to itself, try again.\n");
+				else if (link_matrix[page_num - 1][i] && has_repeated)
+					printf("Page %d already links to page %d, try again.\n", i + 1, page_num);
 				printf("%d. What page does page %d link to? ", j + 1, i + 1);
 				scanf("%d", &page_num);
 				has_repeated = true;
-			} while (page_num < 1 || page_num == i + 1 || page_num > num_pages);
+			} while (page_num < 1 || page_num == i + 1 || link_matrix[page_num - 1][i] || page_num > num_pages);
 
 			// Update link matrix
 			link_matrix[page_num - 1][i] = 1 / (float) num_links;
